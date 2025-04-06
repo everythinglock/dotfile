725e032490e5d050e389f78fa740d212a339a569
fangdong <123fangdong@gmail.com>
2025-04-06 18:10:03 +0800

    init
    fish

---
+++ .config/fish/conf.d/private.fish
@@ -0,0 +1,6 @@
+# avante
+set -x GEMINI_API_KEY "AIzaSyDmbReg2iXA5hUf_kGqKloSW9d7scNrH1o"
+set -x AIHUBMIX_API_KEY "sk-pZv8TC7uaEfdLi5I159b30A96dE149D18fE7D6Bb51D200B8"
+set -x LLM_KEY "183b28693a304014a780a2b5660fd012.afLQdMk1uoglDlYy"
+set -x SILICONFLOW_API_KEY "sk-spsxjcolmmtbqmtqsblskigzjsslcscamyeymfrzxxuxrxng"
+set -x SOPHNET_API_KEY "qksSza5tqsuNWRmpSxPE7EmDA1QzzilxWJUtgQOqkJZn3S0AjPUW58pA8FdnIdqEZHsQyc9Esj-BCEQNpKXDTQ"
